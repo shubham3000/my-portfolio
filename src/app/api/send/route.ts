a765aa46b0cb54e95c507cd016b7ce1194133ade
shubham3000 <shubhamsadhu02@gmail.com>
2025-08-28 01:53:34 +0530

    upadte env

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -2,7 +2,7 @@ import { Resend } from 'resend';
 import { NextResponse } from 'next/server';
 import EmailTemplate from '@/Components/EmailTeamplate/emailTemplate'; 
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
 
 export async function POST(req: Request) {
   try {
